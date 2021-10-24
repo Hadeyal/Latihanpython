176d74a0cf4a73e790ef4ed592e0d3716364d27a
Hadeyal <hadeyalgans@gmail.com>
2021-10-24 12:50:26 +0700

    menambahkan file Belajar1.py

---
+++ Belajar1.py
@@ -0,0 +1,17 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\DELL\AppData\Local\Programs\Python\Python38-32\belajar python-1.py
+Nama                         :Hadeyal
+Nama Panggilan               :Dey
+Tempat dan Tanggal Lahir     :Bekasi 16 Oktober 2004
+Umur                         :17 tahun
+Alamat                       :Kp. Capjaya, Desa. Lenggahsari, Kec. Cabangbungin, Kab. Bekasi 
+Nama Kampus                  :Universitas Pelita Bangsa
+NIM                          :312110293
+Prodi                        :Teknik Informatika
+No Telp                      :085883648213
+assalamu'alaikum
+Perkenalkan nama saya Hadeyal Biasa di panggil Dey Saya lahir di Bekasi 16 Oktober 2004 Saya berumur 17 tahun
+beralamat di Kp. Capjaya, Desa. Lenggahsari, Kec. Cabangbungin, Kab. Bekasi  Saya kuliah di Universitas Pelita Bangsa dengan nim 312110293 saya mengambil prodi Teknik Informatika nomor telepon saya 085883648213
+>>> 
